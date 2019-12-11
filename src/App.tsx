b14f2168687c7d124823bb6d142f341c8b2e3af8
anton <antonkotik0@gmail.com>
2019-12-11 17:19:23 +0200

    - added model for user

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 import { HomePage } from './pages';
 import { Header } from './components';
+import { LoginUser } from './model';
 
 const App: React.FC = () => {
+    const user: LoginUser = { login: '', password: '' };
+
     return (
         <div className="App">
             <Header title="Header title" />
---
+++ src/model/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    name: string;
+    birthday: Date;
+}
+export interface LoginUser {
+    login: string;
+    password: string;
+}
