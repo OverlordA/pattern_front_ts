4044d1880ccd576a0b4f05dc194519bd21b7bd73
anton <antonkotik0@gmail.com>
2020-01-13 10:31:33 +0200

    - added rule for using single quotes for ts code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getAllUsers } from '../api';
 
 const HomePage: React.FC = () => {
+    useEffect(() => {
+        getAllUsers();
+    }, []);
+
     return <div>Home page </div>;
 };
 export default HomePage;
