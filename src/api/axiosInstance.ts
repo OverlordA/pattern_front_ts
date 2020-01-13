f49f4d03714fcb0ea98189e8848d18ac7e7c52f6
anton <antonkotik0@gmail.com>
2020-01-13 11:11:44 +0200

    - Added porxy to package.json

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,4 +3,10 @@ import axios, { AxiosInstance } from 'axios';
 const axiosInstance: AxiosInstance = axios.create({
     timeout: 10000,
 });
+axiosInstance.interceptors.request.use(async request => {
+    request.headers = {
+        'Content-Type': 'application/json',
+    };
+    return request;
+});
 export default axiosInstance;
