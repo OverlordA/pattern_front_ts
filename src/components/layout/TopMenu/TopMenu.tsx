82b7af5773c56fd585655bc4d7322c50661782c3
anton <antonkotik0@gmail.com>
2020-03-13 18:38:49 +0200

    feat: Added csstype and move style for top menu in .ts file

--- src/components/layout/TopMenu/TopMenu.tsx
+++ src/components/layout/TopMenu/TopMenu.tsx
@@ -1,10 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import TopMenuItem from './TopMenuItem/TopMenuItem';
 import pagesLink from './pagesLink';
+import topMenuStyles from './topMenuStyles';
 
 const TopMenu: FunctionComponent = () => {
     return (
-        <div className='top-menu-container'>
+        <div style={topMenuStyles.topMenuContainer}>
             {pagesLink && pagesLink.map(item => <TopMenuItem key={`menu-item-${item.path}`} {...item} />)}
         </div>
     );
---
+++ src/components/layout/TopMenu/topMenuStyles.ts
@@ -0,0 +1,7 @@
+import CSS from 'csstype';
+const topMenuContainer: CSS.Properties = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+};
+export default { topMenuContainer };
